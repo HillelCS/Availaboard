5d656763cdbc79e48b3976433e70c188b07dcac6
Sam Jacobson <cs1@hillelhebrew.org>
2022-03-16 21:34:17 -0700

    Set up grid in webpage
    First webpage was set up. It has a grid with three columns, name, status and contact. Each take a Resource object and take each corresponding method. The AvailaDatabase class was created to load, create and enter objects to and from the database. The methods are currently only a skeleton, the Database needs to be setup before anything else can happen. Finally, the Resource class got a Status and Contact field added. This is because when calling for Status and Contact from the grid the compiler throws an exception when trying to call a method from a subclass of Resource.

--- availaboard-project/src/main/java/com/availaboard/engine/resource/Equipment.java
+++ availaboard-project/src/main/java/com/availaboard/engine/resource/Equipment.java
@@ -1,5 +1,8 @@
 package com.availaboard.engine.resource;
 
 public class Equipment extends Resource {
-
+	@Override
+	public String toString() {
+		return this.getClass().toString();
+	}
 }
--- availaboard-project/src/main/java/com/availaboard/engine/resource/Resource.java
+++ availaboard-project/src/main/java/com/availaboard/engine/resource/Resource.java
@@ -1,5 +1,34 @@
 package com.availaboard.engine.resource;
 
-public abstract class Resource {
+public class Resource {
+	
 	private String name;
+	private Status status;
+	private String contact = "No contact available";
+	
+	private int id;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
 }
