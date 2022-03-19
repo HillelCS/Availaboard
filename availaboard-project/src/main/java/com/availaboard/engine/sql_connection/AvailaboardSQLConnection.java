972257c62e107115050e813efe974ef598086060
Sam Jacobson <cs1@hillelhebrew.org>
2022-03-18 18:22:38 -0700

    Vaadin fix
    Continued vaadin fix

--- availaboard-project/src/main/java/com/availaboard/engine/sql_connection/AvailaboardSQLConnection.java
+++ availaboard-project/src/main/java/com/availaboard/engine/sql_connection/AvailaboardSQLConnection.java
@@ -4,19 +4,21 @@ import java.util.ArrayList;
 
 import com.availaboard.engine.resource.Resource;
 import com.availaboard.engine.resource.Status;
+import com.availaboard.utilitys.ConfigPropReader;
 
 public class AvailaboardSQLConnection {
 	
+	ConfigPropReader config = new ConfigPropReader();
+	public final String username = config.getPropValues().get(0);
+	private final String password = config.getPropValues().get(1);
+	private final String url =  config.getPropValues().get(2);
 	
 	/*
 	 * Load all resources from the database
 	 */
 	public ArrayList<Resource> loadResources() {
 		ArrayList<Resource> arr = new ArrayList<Resource>();
-		Resource res = new Resource();
-		res.setName("Sam Jacobson");
-		res.setStatus(Status.AVAILABLE);
-		arr.add(res);
+		
 		return arr;
 	}
 }
