80ed173748b7e91d10abd4a7e3480c23a0ba46dc
Sam Jacobson <cs1@hillelhebrew.org>
2022-03-18 18:25:03 -0700

    Update ConfigPropReader.java

--- availaboard-project/src/main/java/com/availaboard/utilitys/ConfigPropReader.java
+++ availaboard-project/src/main/java/com/availaboard/utilitys/ConfigPropReader.java
@@ -1,16 +1,17 @@
+
 package com.availaboard.utilitys;
+
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Properties;
+
 public class ConfigPropReader {
 	public ArrayList<String> getPropValues() {
 		ArrayList<String> result = new ArrayList<String>();
 		try {
 			Properties prop = new Properties();
-			File file = new File("C:\\Programming Projects\\Java\\Availaboard\\Availaboard\\availaboard-project\\res\\config.properties");
+			File file = new File("src/main/resources/config.properties");
 			FileInputStream ip = new FileInputStream(file);
 			prop.load(ip);
 			result.add(prop.getProperty("username"));
@@ -19,7 +20,5 @@ public class ConfigPropReader {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		System.out.println(result.get(0) + "FILEFILEFILEFILE");
 		return result;
 	}
-}
