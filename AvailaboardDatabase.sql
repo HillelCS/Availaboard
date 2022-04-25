a27920f6b71e999cd0a7f1abd4688fabc9f2a625
HillelCS <101355933+HillelCS@users.noreply.github.com>
2022-04-25 11:26:37 -0700

    Create AvailaboardDatabase.sql
    Added the SQL dump file to the database with some dummy information in case it needs to get downloaded anywhere.

---
+++ AvailaboardDatabase.sql
@@ -0,0 +1,143 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: availaboard
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `equipment`
+--
+
+DROP TABLE IF EXISTS `equipment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `equipment` (
+  `model` varchar(255) DEFAULT NULL,
+  `equipment_type` varchar(255) DEFAULT NULL,
+  `contact` varchar(255) DEFAULT NULL,
+  `ResourceID` int DEFAULT NULL,
+  `EquipmentID` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`EquipmentID`),
+  KEY `ResourceID` (`ResourceID`),
+  CONSTRAINT `equipment_ibfk_1` FOREIGN KEY (`ResourceID`) REFERENCES `resource` (`ResourceID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `equipment`
+--
+
+LOCK TABLES `equipment` WRITE;
+/*!40000 ALTER TABLE `equipment` DISABLE KEYS */;
+INSERT INTO `equipment` VALUES ('I-pad SE 14 gen','I-pad','Heather Jacobson',5,1);
+/*!40000 ALTER TABLE `equipment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `resource`
+--
+
+DROP TABLE IF EXISTS `resource`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `resource` (
+  `name` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `ResourceID` int NOT NULL AUTO_INCREMENT,
+  `isLoggedIn` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ResourceID`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `resource`
+--
+
+LOCK TABLES `resource` WRITE;
+/*!40000 ALTER TABLE `resource` DISABLE KEYS */;
+INSERT INTO `resource` VALUES ('Sam','BUSY',1,NULL),('John','AVAILABLE',2,NULL),('Heather','BUSY',3,NULL),('Atrium','AVAILABLE',4,NULL),('Heathers I-pad','AVAILABLE',5,NULL),('Kenny','AVAILABLE',6,NULL),('Sobi','BUSY',7,NULL);
+/*!40000 ALTER TABLE `resource` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `room`
+--
+
+DROP TABLE IF EXISTS `room`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `room` (
+  `Contact` varchar(255) DEFAULT NULL,
+  `Location` varchar(255) DEFAULT NULL,
+  `ResourceID` int DEFAULT NULL,
+  `RoomID` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`RoomID`),
+  KEY `ResourceID` (`ResourceID`),
+  CONSTRAINT `room_ibfk_1` FOREIGN KEY (`ResourceID`) REFERENCES `resource` (`ResourceID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `room`
+--
+
+LOCK TABLES `room` WRITE;
+/*!40000 ALTER TABLE `room` DISABLE KEYS */;
+INSERT INTO `room` VALUES ('Rabbi Swigard','Upper west building',4,1);
+/*!40000 ALTER TABLE `room` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `firstName` varchar(255) DEFAULT NULL,
+  `lastName` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `ResourceID` int DEFAULT NULL,
+  `UserID` int NOT NULL AUTO_INCREMENT,
+  `permissions` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`UserID`),
+  UNIQUE KEY `APP_USER_UK` (`username`),
+  KEY `ResourceID` (`ResourceID`),
+  CONSTRAINT `user_ibfk_1` FOREIGN KEY (`ResourceID`) REFERENCES `resource` (`ResourceID`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('Samuel','Jacobson','samjacobson990@gmail.com','samiam','Can\'tGuessThis',1,1,'Admin'),('John','Kaufthal','jkauhftal@khauftal.com','EscapeRoomArtist','Microsoft123',2,2,'User'),('Heather','Jacobson','hljacobson@gmail.com','MotherOfFour','Welcome123',3,3,'User'),('Kenny','Jacobson','kennyjacobs@gmail.com','GreatUncle','Uncle321',6,4,'Admin');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-04-25 11:25:23
