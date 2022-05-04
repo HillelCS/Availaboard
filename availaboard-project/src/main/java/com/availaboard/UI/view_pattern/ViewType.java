56fde18c3980fd7a20298edd42ca8b22f6c9d53f
HillelCS <101355933+HillelCS@users.noreply.github.com>
2022-05-04 10:54:48 -0700

    Added documentation to viewName() interface method

--- availaboard-project/src/main/java/com/availaboard/UI/view_pattern/ViewType.java
+++ availaboard-project/src/main/java/com/availaboard/UI/view_pattern/ViewType.java
@@ -1,5 +1,11 @@
 package com.availaboard.UI.view_pattern;
 
+import com.vaadin.flow.router.Route;
+
 public interface ViewType  {
+     /**
+      * Get's the Views {@link Route} path.
+      * @return The View's {@link Route} path.
+      */
      String viewName();
 }
