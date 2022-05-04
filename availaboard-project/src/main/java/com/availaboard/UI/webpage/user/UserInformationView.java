2a9e2336f6449bb562b213e6e24f34063edaa58c
HillelCS <101355933+HillelCS@users.noreply.github.com>
2022-05-04 11:04:31 -0700

    Added statusIcon to UserInformationView

--- availaboard-project/src/main/java/com/availaboard/UI/webpage/user/UserInformationView.java
+++ availaboard-project/src/main/java/com/availaboard/UI/webpage/user/UserInformationView.java
@@ -4,9 +4,12 @@ import com.availaboard.UI.view_pattern.ViewAuthorization;
 import com.availaboard.UI.view_pattern.ViewConfiguration;
 import com.availaboard.UI.webpage.MainLayout;
 import com.availaboard.engine.resource.Permission;
+import com.availaboard.engine.resource.Status;
 import com.availaboard.engine.security.AccessControl;
 import com.availaboard.engine.security.AccessControlFactory;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
@@ -22,11 +25,14 @@ public class UserInformationView extends VerticalLayout implements ViewAuthoriza
      */
     private static final long serialVersionUID = -8469495034991926228L;
     private final Label usernameLabel = new Label();
+    private final Icon statusIcon;
 
     private final AccessControl accessControl;
 
     public UserInformationView() {
         accessControl = AccessControlFactory.getInstance().createAccessControl();
+        statusIcon = accessControl.getCurrentUser().getStatus() == Status.AVAILABLE ? new Icon(VaadinIcon.CHECK_CIRCLE_O) : new Icon(VaadinIcon.CLOSE_CIRCLE_O);
+        setHorizontalComponentAlignment(Alignment.START, usernameLabel, statusIcon);
     }
 
 
@@ -41,7 +47,7 @@ public class UserInformationView extends VerticalLayout implements ViewAuthoriza
 
     @Override
     public void addAll() {
-
+        add(usernameLabel, statusIcon);
     }
 
     @Override
