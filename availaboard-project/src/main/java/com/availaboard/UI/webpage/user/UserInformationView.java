a22118d76a6b15246b3a63ab00fb1438f59f09c3
Sam Jacobson <cs1@hillelhebrew.org>
2022-05-06 15:32:13 -0700

    Added container and CSS to username and status label in UserInformationView
    The VerticalLayout container was used so they could both be aligned in the center of it, making them aligned directly on top of each-other. The text also have auto-adjusting size depending on the viewport sizes.

--- availaboard-project/src/main/java/com/availaboard/UI/webpage/user/UserInformationView.java
+++ availaboard-project/src/main/java/com/availaboard/UI/webpage/user/UserInformationView.java
@@ -5,18 +5,15 @@ import com.availaboard.UI.view_pattern.ViewAuthorization;
 import com.availaboard.UI.view_pattern.ViewConfiguration;
 import com.availaboard.UI.webpage.MainLayout;
 import com.availaboard.engine.resource.Permission;
-import com.availaboard.engine.resource.Status;
 import com.availaboard.engine.security.AccessControl;
 import com.availaboard.engine.security.AccessControlFactory;
 import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Label;
-import com.vaadin.flow.component.icon.Icon;
-import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
 import java.util.stream.Stream;
+
 @CssImport("./styles/webpage-styles/user-information-view.css")
 @Route(value = UserInformationView.VIEWNAME, layout = MainLayout.class)
 public class UserInformationView extends VerticalLayout implements ViewAuthorization, ViewConfiguration {
@@ -29,6 +26,8 @@ public class UserInformationView extends VerticalLayout implements ViewAuthoriza
     private static final long serialVersionUID = -8469495034991926228L;
     private Label usernameLabel;
     private Label statusLabel;
+
+    private final VerticalLayout userStatusContainer = new VerticalLayout();
     private final AccessControl accessControl;
 
     public UserInformationView() {
@@ -42,8 +41,10 @@ public class UserInformationView extends VerticalLayout implements ViewAuthoriza
 
         usernameLabel.addClassName("username-label");
         statusLabel.addClassName("status-label");
-
-        setHorizontalComponentAlignment(Alignment.START, usernameLabel, statusLabel);
+        userStatusContainer.addClassName("username-status-container");
+        userStatusContainer.setAlignItems(Alignment.CENTER);
+        userStatusContainer.add(usernameLabel, statusLabel);
+        setHorizontalComponentAlignment(Alignment.START, userStatusContainer);
     }
 
 
@@ -58,7 +59,7 @@ public class UserInformationView extends VerticalLayout implements ViewAuthoriza
 
     @Override
     public void addAll() {
-        add(usernameLabel, statusLabel);
+        add(userStatusContainer);
     }
 
     @Override
