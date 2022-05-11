6cb1433c21f81f71c7160ab319ff84eae6a80586
Sam Jacobson <cs1@hillelhebrew.org>
2022-05-10 20:18:51 -0700

    Update ViewConfiguration.java
    Corrected documentation. It was referencing an outdated name of the method.

--- availaboard-project/src/main/java/com/availaboard/UI/application_structure/view_structure/ViewConfiguration.java
+++ availaboard-project/src/main/java/com/availaboard/UI/application_structure/view_structure/ViewConfiguration.java
@@ -5,13 +5,14 @@ import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
 
 public interface ViewConfiguration extends ViewType, BeforeEnterObserver {
+
     /**
      * Use this method to add components to the View.
      */
     void initialize();
 
     /**
-     * Calls the addAll() method before the page loads up. Also
+     * Calls the initialize() method before the page loads up. Also
      * set's the route as the getViewName() implementation and the
      * ParentLayout as the {@link MainLayout}.
      *
