0c2549e8c3f56809cd1fa2e442a919ef523c71af
Sam Jacobson <cs1@hillelhebrew.org>
2022-05-25 20:55:28 -0700

    Update AvailaboardDatabase.sql

--- AvailaboardDatabase.sql
+++ AvailaboardDatabase.sql
@@ -31,7 +31,7 @@ CREATE TABLE `equipment` (
   PRIMARY KEY (`EquipmentID`),
   KEY `ResourceID` (`ResourceID`),
   CONSTRAINT `equipment_ibfk_1` FOREIGN KEY (`ResourceID`) REFERENCES `resource` (`ResourceID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +40,7 @@ CREATE TABLE `equipment` (
 
 LOCK TABLES `equipment` WRITE;
 /*!40000 ALTER TABLE `equipment` DISABLE KEYS */;
-INSERT INTO `equipment` VALUES ('I-pad SE 14 gen','Computer','John Smith',5,1);
+INSERT INTO `equipment` VALUES ('MINI','I-PAD','Jonny',38,2);
 /*!40000 ALTER TABLE `equipment` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -56,7 +56,7 @@ CREATE TABLE `resource` (
   `status` varchar(255) DEFAULT NULL,
   `ResourceID` int NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`ResourceID`)
-) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -65,7 +65,7 @@ CREATE TABLE `resource` (
 
 LOCK TABLES `resource` WRITE;
 /*!40000 ALTER TABLE `resource` DISABLE KEYS */;
-INSERT INTO `resource` VALUES ('samiam','BUSY',1),('Heather','BUSY',3),('Atrium','AVAILABLE',4),('Heathers I-pad','AVAILABLE',5),('Kenny','AVAILABLE',6);
+INSERT INTO `resource` VALUES ('samiam','BUSY',1),('unpluggedsam','AVAILABLE',35),('Atrium','AVAILABLE',36),('Atrium','AVAILABLE',37),('Jonnys Ipad','BUSY',38);
 /*!40000 ALTER TABLE `resource` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -84,7 +84,7 @@ CREATE TABLE `room` (
   PRIMARY KEY (`RoomID`),
   KEY `ResourceID` (`ResourceID`),
   CONSTRAINT `room_ibfk_1` FOREIGN KEY (`ResourceID`) REFERENCES `resource` (`ResourceID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -93,7 +93,7 @@ CREATE TABLE `room` (
 
 LOCK TABLES `room` WRITE;
 /*!40000 ALTER TABLE `room` DISABLE KEYS */;
-INSERT INTO `room` VALUES ('Zachary','Upper west building',4,1);
+INSERT INTO `room` VALUES ('Sam','West building',36,2),('Josh','Upper-west',37,3);
 /*!40000 ALTER TABLE `room` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -117,7 +117,7 @@ CREATE TABLE `user` (
   UNIQUE KEY `APP_USER_UK` (`username`),
   KEY `ResourceID` (`ResourceID`),
   CONSTRAINT `user_ibfk_1` FOREIGN KEY (`ResourceID`) REFERENCES `resource` (`ResourceID`)
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -126,8 +126,35 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES ('AccountOne','Park','contact@gmail.com','samiam','Can\'tGuessThis',1,1,'Admin'),('John','Smith','john@gmail.com','FatherOfFour','Welcome123',3,3,'User'),('Kenny','Terreek','kenntereeks@gmail.com','GreatUncle','Uncle321',6,4,'Admin');
+INSERT INTO `user` VALUES ('AccountOne','Park','contact@gmail.com','samiam','Can\'tGuessThis',1,1,'Admin'),('Samuel','Jacobson','unpluggedsam990@gmail.com','unpluggedsam','woohoo',35,28,'Admin');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `visible_in_grid`
+--
+
+DROP TABLE IF EXISTS `visible_in_grid`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `visible_in_grid` (
+  `visibility` varchar(255) DEFAULT NULL,
+  `ResourceID` int DEFAULT NULL,
+  `visibilityID` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`visibilityID`),
+  KEY `ResourceID` (`ResourceID`),
+  CONSTRAINT `visible_in_grid_ibfk_1` FOREIGN KEY (`ResourceID`) REFERENCES `resource` (`ResourceID`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `visible_in_grid`
+--
+
+LOCK TABLES `visible_in_grid` WRITE;
+/*!40000 ALTER TABLE `visible_in_grid` DISABLE KEYS */;
+INSERT INTO `visible_in_grid` VALUES ('1',1,2),('1',35,3),('1',37,4),('1',38,5);
+/*!40000 ALTER TABLE `visible_in_grid` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -139,4 +166,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-05-24 21:26:35
+-- Dump completed on 2022-05-25 20:55:15
